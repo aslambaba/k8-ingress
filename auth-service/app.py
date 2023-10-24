bcbc24cc80d8b934b9cd749951c74d292ee9f3a2
mahouESPRIT <ayoub.mahou@esprit.tn>
2023-10-24 13:53:19 +0100

    Update With Lua Script

--- auth-service/app.py
+++ auth-service/app.py
@@ -1,29 +1,24 @@
 from flask import Flask, request, jsonify
 import redis
-import requests
 
 app = Flask(__name__)
 redis_client = redis.StrictRedis(host='redis', port=6379, db=0)
 
-@app.route('/authenticate', methods=['POST'])
-def authenticate():
-    key = request.form['key']
+@app.route('/dynamic/<key>', methods=['POST', 'GET'])
+def dynamic_routing(key):
+    redis_key = key  # No need to concatenate the value to the key
+    pod_name = redis_client.get(redis_key)
 
-    pod_name = redis_client.get(key)
-    
     if pod_name:
         pod_name = pod_name.decode('utf-8')
-        
-        # Route traffic to the 'dynamic-pods' service with the selected pod_name
-        response = requests.get(f"http://{pod_name}.default.svc.cluster.local", params={'key': key})
-        
-        # Return the response from the specific pod
-        return response.text, response.status_code
+        return pod_name, 200  # Return the value (pod name) based on the key
     else:
         return "Not Found", 404
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
+
+
 
 
 
